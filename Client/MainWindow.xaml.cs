90acf557004b4630b2f17c51cd07c4bf262df950
ChukhnenkoDA <CiberKnight2018@gmail.com>
2024-07-30 22:22:24 +0300

    FHFRONT-2

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -14,27 +14,34 @@ namespace Client {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        LoginWindow login;
+        RegisterWindow register;
+
         public MainWindow() {
             InitializeComponent();
         }
 
         private void Button_Login(object sender, RoutedEventArgs e) {
-
+            login = new LoginWindow(this, new());
+            this.Visibility = Visibility.Hidden;
+            login.ShowDialog();
         }
 
         private void Button_Register(object sender, RoutedEventArgs e) {
-
+            register = new RegisterWindow(this, new());
+            this.Visibility = Visibility.Hidden;
+            register.ShowDialog();
         }
 
         public void GoBack(System.Windows.Window window) {
             Visibility = Visibility.Visible;
-            if (create is not null && create != window) create.Close();
-            else if (lobby is not null && lobby != window) lobby.Close();
+            if (login is not null && login != window) login.Close();
+            else if (register is not null && register != window) register.Close();
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
-            if (create is not null) create.Close();
-            else if (lobby is not null) lobby.Close();
+            if (login is not null) login.Close();
+            else if (register is not null) register.Close();
         }
     }
 }
---
+++ Client/RegisterWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        MainWindow mainWindow;
+        RegisterWindowModel Model { get; set; }
+
+        public RegisterWindow(MainWindow mainWindow, RegisterWindowModel model) {
+            InitializeComponent();
+
+            this.mainWindow = mainWindow;
+            Model = model;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) => mainWindow.GoBack(this);
+    }
+}
