2af5e75aa0eb326d29efb7341a12e434c608c97f
ChukhnenkoDA <CiberKnight2018@gmail.com>
2024-08-08 20:23:55 +0300

    FHFRONT-5

--- Client/EmailCheckWindow.xaml.cs
+++ Client/EmailCheckWindow.xaml.cs
@@ -74,6 +74,7 @@ namespace Client
                     window.ShowDialog();
                 }
                 else if (Model.IsFromRegistration) {
+                    Model.IsFromRegistration = false;
                     mainWindow.GoBack(this);
                 }
             }
