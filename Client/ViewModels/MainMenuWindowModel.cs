12e5351f5d8bce6ed71326d54a045ef37d66727c
ChukhnenkoDA <CiberKnight2018@gmail.com>
2024-08-22 18:20:17 +0300

    Исправление ошибок FHFRONT-5

--- Client/ViewModels/MainMenuWindowModel.cs
+++ Client/ViewModels/MainMenuWindowModel.cs
@@ -20,37 +20,49 @@ namespace Client {
             set => SetProperty(ref files, value);
         }
 
+        private List<File> allFiles;
+        public List<File> AllFiles {
+            get => allFiles;
+            set => SetProperty(ref allFiles, value);
+        }
+
+        public IReadOnlyList<string> AllFilesString {
+            get => allFiles.Select(File => File.Path).ToArray();
+        }
+
         private string search;
         public string Search {
             get => search;
             set => SetProperty(ref search, value);
         }
 
-        private FileModel selectedFolder;
-        public FileModel SelectedFolder {
-            get => selectedFolder;
-            set => SetProperty(ref selectedFolder, value);
+        private int selectedFolderId;
+        public int SelectedFolderId {
+            get => selectedFolderId;
+            set => SetProperty(ref selectedFolderId, value);
         }
 
-        public MainMenuWindowModel(User user, TcpClient client, List<FileModel> files, string search, FileModel selectedFolder) {
+        public MainMenuWindowModel(User user, TcpClient client, List<FileModel> files, string search, int selectedFolderId) {
             User = user;
             Client = client;
             Files = files;
             Search = search;
-            SelectedFolder = selectedFolder;
+            SelectedFolderId = selectedFolderId;
         }
 
         public MainMenuWindowModel(User user, TcpClient client) {
             User = user;
             Client = client;
             Files = new List<FileModel>();
+            AllFiles = new List<File>();
             Task.Run(() => Update());
             Search = "";
-            SelectedFolder = null;
+            SelectedFolderId = -1;
         }
 
         public async Task Update() {
-            Files = (await Task.Run(async () => await ClientCommands.GetAllFilesAsync(Client, User, (SelectedFolder.File.Id == null ? -1 : SelectedFolder.File.Id) ))).Select((File) => new FileModel(File)).ToList();
+            AllFiles = (List<File>) await Task.Run(async () => await ClientCommands.GetAllFilesAsync(Client, User)); // Тут будет команда на получение вообще всех файлов
+            Files = AllFiles.Where(File => File.ParentId == SelectedFolderId).Select((File) => new FileModel(File)).ToList();
         }
     }
 }
