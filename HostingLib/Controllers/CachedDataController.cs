dbb6cf708675ecb153f085e9035f85f642e3d528
OptiRus <tylerthecatoc@gmail.com>
2024-08-23 14:31:59 +0300

    Добавил возможность работы с редисом
    FHBACK-12

---
+++ HostingLib/Controllers/CachedDataController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HostingLib.Classes;
+using HostingLib.Helpers;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+
+namespace HostingLib.Controllers
+{
+    public class CachedDataController
+    {
+        private static readonly IDatabase redis = RedisConnectionHelper.Connection.GetDatabase();
+
+        public static async Task<FileDetails> GetFileDetailsAsync(int file_id)
+        {
+            string cache_key = $"file:details:{file_id}";
+
+            RedisValue cached_details = await redis.StringGetAsync(cache_key);
+
+            return !cached_details.IsNullOrEmpty ? JsonConvert.DeserializeObject<FileDetails>(cached_details) : null;
+
+        }
+
+        public static async Task SetFileDetailsAsync(int file_id, FileDetails details)
+        {
+            string cache_key = $"file:details:{file_id}";
+            await redis.StringSetAsync(cache_key, JsonConvert.SerializeObject(details), TimeSpan.FromHours(1));
+        }
+    }
+}
---
+++ HostingLib/Helpers/RedisConnectionHelper.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostingLib.Helpers
+{
+    public class RedisConnectionHelper
+    {
+        private static Lazy<ConnectionMultiplexer> lazy_connection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            return ConnectionMultiplexer.Connect("localhost");
+        });
+
+        public static ConnectionMultiplexer Connection => lazy_connection.Value;
+    }
+}
