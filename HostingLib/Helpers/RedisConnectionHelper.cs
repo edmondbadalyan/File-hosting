cc9863a741d615f99d53db29e1f672bcfc823ded
OptiRus <tylerthecatoc@gmail.com>
2024-08-30 16:18:09 +0300

    Доработал удаление и стирание файлов - теперь рекурсия должна работать правильно
    FHBACK-1

--- HostingLib/Helpers/RedisConnectionHelper.cs
+++ HostingLib/Helpers/RedisConnectionHelper.cs
@@ -1,6 +1,8 @@
-﻿using StackExchange.Redis;
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
